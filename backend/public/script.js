b2fac8acd78b090a38a8f2382901548376328b44
virajj12 <vjvirajjain122005@gmail.com>
2025-10-23 21:56:32 +0530

    Update script.js

--- backend/public/script.js
+++ backend/public/script.js
@@ -25,7 +25,7 @@ const fileInput = document.getElementById('notificationImage');
 const imagePreview = document.getElementById('imagePreview');
 
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://college-connectt.onrender.com';
 
 // Predefined users for demonstration
 /*const initialUsers = {
